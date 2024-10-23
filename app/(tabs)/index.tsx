bb6b793f913aa35159af50fe0e9dcba23195cef1
sittes <paulo.fagnerdeoliveira@gmail.com>
2024-10-23 14:14:02 -0300

    primeiro commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet, Platform,Text,View,Button } from 'react-native';
+
+
+
+
+export default function HomeScreen() {
+  return (
+   <View style={styles.Container}>
+    <Text style={styles.titleContainer}>Minha jornada no react native</Text>
+    <Button   title="Criar conta" onPress={() => console.log("Button Pressed!")} />
+   </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    borderBlockColor: '#f64348',
+    borderBottomWidth: 1,
+    fontSize: 60,
+    color: '#f64348',
+  },
+  Container: {
+   flex: 1,
+   backgroundColor: '#fff',
+   alignItems: 'flex-start',
+   justifyContent: 'center',
+   padding: 80,
+  },
+});
