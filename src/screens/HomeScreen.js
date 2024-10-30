622b0ba863d0ec552574d5bd2530a0e724ea458e
sittes <paulo.fagnerdeoliveira@gmail.com>
2024-10-29 23:42:24 -0300

    alterando projeto

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -18,6 +18,7 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center",
         color: "#64348",
+        fontSize: 40,
     },
     text: {
         fontSize: 40,
