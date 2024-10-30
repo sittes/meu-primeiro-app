65d7fd9e10d42e3a67aa92c347aed29de9deb8fe
sittes <paulo.fagnerdeoliveira@gmail.com>
2024-10-30 08:36:31 -0300

    alterando projeto

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -14,7 +14,7 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "red",
+        backgroundColor: "white",
         alignItems: "center",
         justifyContent: "center",
 
