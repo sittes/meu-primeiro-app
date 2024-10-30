8e14bb247ff8186435653ca8ac1ed31cf3774e07
sittes <paulo.fagnerdeoliveira@gmail.com>
2024-10-30 08:37:33 -0300

    alterando projeto

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,6 +6,7 @@ export default function HomeScreen() {
         <View style={styles.container}>
             <Text style={styles.text}>Ola Mundo</Text>
             <Text style={styles.text}>Ola Mundo</Text>
+            <Text style={styles.text}>a Mundo</Text>
             <StatusBar style="auto" />
         </View>
     );
